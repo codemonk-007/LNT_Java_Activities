b463ec7e2a2aab31ca80fa06ed6753d41f595db3
Jay Singh <81297719+codemonk-007@users.noreply.github.com>
2021-06-07 21:25:17 +0530

    Delete Activity2 directory

--- Activity2/Hotel/DeluxeACRoom.java
+++
@@ -1,9 +0,0 @@
-package Hotel;
-
-public class DeluxeACRoom extends DeluxeRoom {
-	public DeluxeACRoom(String hotelName, int numberOfSqFeet, boolean hasTV, boolean hasWifi) {
-		super(hotelName, numberOfSqFeet, hasTV, hasWifi);
-		this.ratePerSqFeet = 12;
-	}
-
-}
